fa3bc97ebe8a074759b1b3631992133c9b9296fd
woocashpirowski <cashpirowski@gmail.com>
2021-05-16 13:38:29 +0200

    showcase, module aliases

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Witaj na ceremonii</h1>
+      <h2>Znajdź najlepsze wydarzenia w Twojej okolicy</h2>
+    </div>
+  )
+}
