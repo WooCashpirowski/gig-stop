105c3089a7c27ae99b02549d2f8d03a927588f8d
woocashpirowski <cashpirowski@gmail.com>
2021-05-16 10:38:49 +0200

    404

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import { FaExclamationTriangle } from 'react-icons/fa'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import styles from '../styles/404.module.css'
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Nie znaleziono strony">
+      <div className={styles.error}>
+        <h1>
+          {' '}
+          <FaExclamationTriangle /> 404
+        </h1>
+        <h4>Sorry, nic tu nie ma.</h4>
+        <Link href="/">Powrót do strony głównej</Link>
+      </div>
+    </Layout>
+  )
+}
